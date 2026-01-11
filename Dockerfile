66507a69a0b9f31c038bdbaa8534db1571dca22f
Adityaprasad02 <shubhamsahoo230@gmail.com>
2026-01-11 23:16:30 +0530

    applied spring profiles for dev and prod

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# syntax=docker/dockerfile:1
+
+FROM maven:3.9.9-eclipse-temurin-21-alpine AS build
+
+WORKDIR /app
+
+COPY pom.xml .
+
+RUN mvn dependency:go-offline -B
+
+COPY src ./src
+
+RUN mvn clean package -DskipTests
+
+FROM eclipse-temurin:21-jre-jammy
+
+WORKDIR /app
+
+COPY --from=build /app/target/*.jar  myapp.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java" , "-jar"]
+
+CMD ["myapp.jar"]
+
--- src/main/java/com/Examlens_Backend/FileHandler/Config/configuration.java
+++ src/main/java/com/Examlens_Backend/FileHandler/Config/configuration.java
@@ -1,15 +1,20 @@
 package com.Examlens_Backend.FileHandler.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.concurrent.Executor;
 
 @Configuration
 public class configuration
 {
+    @Value("${frontend.url}")
+    private String frontend_url ;
 
     @Bean
     public RestTemplate restTemplate(){
